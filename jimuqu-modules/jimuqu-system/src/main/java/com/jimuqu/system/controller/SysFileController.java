520782464187ddac0ce7e03887580cde53f780f4
chengliang <chengliang4810@163.com>
2026-07-18 05:32:06 +0800

    fix: 保留 OSS 上传扩展信息

--- jimuqu-modules/jimuqu-system/src/main/java/com/jimuqu/system/controller/SysFileController.java
+++ jimuqu-modules/jimuqu-system/src/main/java/com/jimuqu/system/controller/SysFileController.java
@@ -1,9 +1,11 @@
 package com.jimuqu.system.controller;
 
+import cn.hutool.core.lang.Dict;
 import cn.dev33.satoken.annotation.SaCheckPermission;
 import com.jimuqu.common.core.checker.Assert;
 import com.jimuqu.common.log.annotation.Log;
 import com.jimuqu.common.log.enums.BusinessType;
+import com.jimuqu.common.core.utils.JsonUtil;
 import com.jimuqu.common.mybatis.core.Page;
 import com.jimuqu.common.mybatis.core.page.PageQuery;
 import com.jimuqu.common.web.core.BaseController;
@@ -14,12 +16,14 @@ import com.jimuqu.system.service.SysFileService;
 import lombok.RequiredArgsConstructor;
 import org.dromara.x.file.storage.core.FileInfo;
 import org.dromara.x.file.storage.core.FileStorageService;
+import org.dromara.x.file.storage.core.upload.UploadPretreatment;
 import org.noear.solon.annotation.Controller;
 import org.noear.solon.annotation.Delete;
 import org.noear.solon.annotation.Get;
 import org.noear.solon.annotation.Mapping;
 import org.noear.solon.annotation.Post;
 import org.noear.solon.core.handle.DownloadedFile;
+import org.noear.solon.core.handle.Context;
 import org.noear.solon.core.handle.UploadedFile;
 import org.noear.solon.validation.annotation.NoRepeatSubmit;
 import org.noear.solon.validation.annotation.NotEmpty;
@@ -56,9 +60,17 @@ public class SysFileController extends BaseController {
     @NoRepeatSubmit
     @SaCheckPermission("system:oss:upload")
     @Log(title = "OSS对象存储", businessType = BusinessType.UPLOAD)
-    public SysOssUploadVo upload(UploadedFile file) {
-        FileInfo info = fileStorageService.of(
-                file.getContent(), file.getName(), file.getContentType(), file.getContentSize()).upload();
+    public SysOssUploadVo upload(UploadedFile file, String ossExt) {
+        UploadPretreatment upload = fileStorageService.of(
+                file.getContent(), file.getName(), file.getContentType(), file.getContentSize());
+        if (ossExt != null && !ossExt.isBlank()) {
+            Dict ext = JsonUtil.toObject(ossExt, Dict.class);
+            ext.set("fileSize", file.getContentSize());
+            ext.set("contentType", file.getContentType());
+            ext.set("uploadIp", Context.current().realIp());
+            upload.setAttr(ext);
+        }
+        FileInfo info = upload.upload();
         Assert.notNull(info, "文件上传失败");
         return new SysOssUploadVo(info.getUrl(), info.getOriginalFilename(), info.getId());
     }
